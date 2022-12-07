1c23ecd0822dbb06faae4fe578decbcfa489c867
Houda Jawad <houdajawad@gmail.com>
2022-12-06 18:31:12 -0800

    Add methods to the different arrays

--- arrays.js
+++ arrays.js
@@ -1,6 +1,22 @@
 var friends = ["Jo","Chris","Ash","Linda"];
+friends.pop();
 console.log(friends);
+
+// I used the pop() method for the array of string elements.
+// This method removes the last element from and array.
+// The output is [ 'Jo', 'Chris', 'Ash' ].
+
 var ages =[23,24,27,30];
-console.log(ages)
+ages.push(70,35);
+console.log(ages);
+// I used the push() method for the array of integer elements.
+// This method adds one or more element to the end of the array.
+// The output is [ 23, 24, 27, 30, 70, 35 ].
+
 var likeCakes = [false,true,true,false];
-console.log(likeCakes)
+likeCakes.shift();
+console.log(likeCakes);
+
+// I used the shift() method for the array of boolean elements.
+// This method removes the first element from an array.
+// The output is [ true, true, false ].
